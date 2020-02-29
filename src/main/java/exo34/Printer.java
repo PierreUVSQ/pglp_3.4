42489b982c2dba89496027cbe536de4de4188b14
plicence <p.licencedev@gmail.com>
2020-02-29 19:54:15 +0100

    First commit

---
+++ src/main/java/exo34/Printer.java
@@ -0,0 +1,9 @@
+package exo34;
+
+public interface Printer {
+
+	public void print() ;
+	public void scan() ;
+	public void copy() ;
+	public void fax() ;
+}
---
+++ src/main/java/exo34/SimplePrinter.java
@@ -0,0 +1,22 @@
+package exo34;
+
+public class SimplePrinter implements Printer{
+
+	@Override
+	public void print() {};
+	
+	@Override
+	public void scan() {
+		
+	}
+	@Override
+	public void copy() {
+		
+		
+	}
+	
+	@Override
+	public void fax() {
+		
+	}
+}
